1c47193ab8f9b74d7e13d1562b3751b9f5ecdaff
edekeemmanuel <edekeemmanuel55@gmail.com>
2024-03-24 17:38:07 +0100

    pcts

--- src/routes/pathConstants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PathConstants = void 0;
-exports.PathConstants = {
-    HOME: "/",
-    // other pages's paths...
-    EPROFILE: "profile",
-    ABOUT: "about"
-};
